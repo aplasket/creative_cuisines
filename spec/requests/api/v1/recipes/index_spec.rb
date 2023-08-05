ca0dd1b80c2f10bc0a3457832c36052e0935f4bf
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-05 14:07:08 -0400

    add initial WIP test for endpoint 1

---
+++ spec/requests/api/v1/recipes/index_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "Recipes Index Request" do
+  describe "fetches all recipes" do
+    it "requests recipes from a specific country" do
+      get "/api/v1/recipes?country=thailand"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+      binding.pry
+
+    end
+  end
+end
