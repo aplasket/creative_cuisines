025a7def12a429224a21fd23bdf051e3d6b5c9b2
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-05 15:26:05 -0400

    refactor with facade, service and poro

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,21 +1,6 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-
-    # render json: RecipeSerializer.all(recipes)
-    binding.pry
-    country = params[:q]
-    conn = Faraday.new(url: "https://api.edamam.com") do |faraday|
-      faraday.params['app_id'] = ENV["EDAMAM_ID"]
-      faraday.params['app_key'] = ENV['EDAMAM_KEY']
-      faraday.params['type'] = "any"
-      faraday.params['q'] = country
-    end
-
-    response = conn.get("/api/recipes/v2")
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    @recipes = json[:hits].map do |recipe_data|
-      Recipe.new(recipe_data)
-    end
+    @facade = RecipeFacade.new(params[:country]).recipes
+    render json: RecipeSerializer.new(@facade)
   end
 end
