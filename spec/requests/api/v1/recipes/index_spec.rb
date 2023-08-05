d68eab379b231f0ad4d2ac7d0afba6827fc8fadb
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-05 15:26:49 -0400

    update tests

--- spec/requests/api/v1/recipes/index_spec.rb
+++ spec/requests/api/v1/recipes/index_spec.rb
@@ -9,8 +9,33 @@ RSpec.describe "Recipes Index Request" do
       expect(response.status).to eq(200)
 
       recipes = JSON.parse(response.body, symbolize_names: true)
-      binding.pry
 
+      expect(recipes).to have_key(:data)
+      expect(recipes[:data]).to be_an(Array)
+      expect(recipes[:data].count).to eq(20)
+
+      recipes[:data].map do |recipe|
+        expect(recipe).to have_key(:id)
+        expect(recipe[:id]).to eq(nil)
+
+        expect(recipe).to have_key(:type)
+        expect(recipe[:type]).to eq("recipe")
+
+        expect(recipe).to have_key(:attributes)
+        expect(recipe[:attributes]).to be_a(Hash)
+
+        expect(recipe[:attributes]).to have_key(:title)
+        expect(recipe[:attributes][:title]).to be_a(String)
+
+        expect(recipe[:attributes]).to have_key(:url)
+        expect(recipe[:attributes][:url]).to be_a(String)
+
+        expect(recipe[:attributes]).to have_key(:country)
+        expect(recipe[:attributes][:country]).to be_a(String)
+
+        expect(recipe[:attributes]).to have_key(:image)
+        expect(recipe[:attributes][:image]).to be_a(String)
+      end
     end
   end
 end
