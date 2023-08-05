5c76a44d815b1d63eb982611078a824985b7044c
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-05 17:32:46 -0400

    update index controller with country facade and conditionals

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    @facade = RecipeFacade.new(params[:country]).recipes
-    render json: RecipeSerializer.new(@facade)
+    country = CountryFacade.new(params).get_country
+    facade = RecipeFacade.new(country).recipes
+    if facade.nil?
+      render json: { "data": [] }
+    else
+      render json: RecipeSerializer.new(facade)
+    end
   end
 end
