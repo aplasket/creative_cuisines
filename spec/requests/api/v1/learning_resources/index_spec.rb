d31a293b293200264803986a541e28dab53d10a2
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-05 21:35:31 -0400

    add in tests for no search results rendered for video and images

--- spec/requests/api/v1/learning_resources/index_spec.rb
+++ spec/requests/api/v1/learning_resources/index_spec.rb
@@ -38,5 +38,36 @@ RSpec.describe "Learning Resources Index Request" do
       expect(data[:data][:attributes][:images].first).to have_key(:url)
       expect(data[:data][:attributes][:images].first[:url]).to be_a(String)
     end
+
+    it "if no videos or images are found, those keys point to an empty object", :vcr do
+      params = {country: "qz_"}
+      get "/api/v1/learning_resources?country=#{params[:country]}"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data).to have_key(:data)
+      expect(data[:data]).to be_a(Hash)
+
+      expect(data[:data]).to have_key(:id)
+      expect(data[:data][:id]).to eq(nil)
+
+      expect(data[:data]).to have_key(:type)
+      expect(data[:data][:type]).to eq("learning_resource")
+
+      expect(data[:data]).to have_key(:attributes)
+      expect(data[:data][:attributes]).to have_key(:country)
+      expect(data[:data][:attributes][:country]).to eq(params[:country])
+
+      expect(data[:data][:attributes]).to have_key(:video)
+      expect(data[:data][:attributes][:video]).to be_a(Hash)
+      expect(data[:data][:attributes][:video]).to eq({})
+
+      expect(data[:data][:attributes]).to have_key(:images)
+      expect(data[:data][:attributes][:images]).to be_an(Array)
+      expect(data[:data][:attributes][:images]).to eq([])
+    end
   end
 end
