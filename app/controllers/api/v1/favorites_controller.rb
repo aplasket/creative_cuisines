9a2bb70b2cd631b9462326a9ce6a1207d6510c7f
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-06 15:57:05 -0400

    add sad paths and update in create action

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::FavoritesController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
     if user
-      Favorite.create(favorite_params)
+      new_favorite = Favorite.create(favorite_params)
+      new_favorite.update!(user_id: user.id)
       render json: { success: "Favorite added successfully" }, status: 201
     else
       render json: { error: "Unauthorized user" }, status: 400
--- spec/requests/api/v1/favorites/create_request_spec.rb
+++ spec/requests/api/v1/favorites/create_request_spec.rb
@@ -27,4 +27,29 @@ RSpec.describe "Favorite Create Action Endpoint" do
       expect(user[:success]).to eq("Favorite added successfully")
     end
   end
+
+  describe "sad paths" do
+    it "displays an error if api key is invalid" do
+        user_params = {
+          "api_key": "jgn983hy48thw9begh98h4539h4",
+          "country": "thailand",
+          "recipe_link": "https://www.tastingtable.com/.....",
+          "recipe_title": "Crab Fried Rice (Khaao Pad Bpu)"
+          }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post "/api/v1/favorites", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+
+        user = JSON.parse(response.body, symbolize_names: true)
+
+        expect(user).to be_a(Hash)
+        expect(user).to have_key(:error)
+        expect(user[:error]).to be_a(String)
+        expect(user[:error]).to eq("Unauthorized user")
+    end
+  end
 end
