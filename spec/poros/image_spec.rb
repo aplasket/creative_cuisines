2da6f9c698d9541dbc48af2af240e85ba67bd61c
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-05 21:52:49 -0400

    update image spec add video spec

--- spec/poros/image_spec.rb
+++ spec/poros/image_spec.rb
@@ -5,15 +5,9 @@ RSpec.describe Image do
     params = {country: "laos"}
     resources = ResourceFacade.new(params)
 
-    expect(resources.country).to eq(params[:country])
-
     expect(resources.images).to be_an(Array)
     expect(resources.images.first).to be_an(Image)
     expect(resources.images.first.alt_tag).to be_a(String)
     expect(resources.images.first.url).to be_a(String)
-
-    expect(resources.video).to be_a(Video)
-    expect(resources.video.title).to be_a(String)
-    expect(resources.video.youtube_video_id).to be_a(String)
   end
 end
