7383698531e58876444fb356b56064df35e41680
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-06 14:14:01 -0400

    add tests for create action sessions

---
+++ spec/requests/api/v1/sessions/create_request_spec.rb
@@ -0,0 +1,74 @@
+require "rails_helper"
+
+RSpec.describe "Sessions Create Request" do
+  describe "happy path" do
+    it "logs in a user" do
+      user = User.create!(name: "Ringo", email: "starr@ruffruff.com", password: "treats123", password_confirmation: "treats123" )
+
+      user_params = {
+        "email": "starr@ruffruff.com",
+        "password": "treats123"
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post "/api/v1/sessions", headers: headers, params: JSON.generate(user_params)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(user).to have_key(:data)
+      expect(user[:data]).to be_a(Hash)
+      expect(user[:data]).to have_key(:id)
+      expect(user[:data][:id]).to be_a(String)
+
+      expect(user[:data]).to have_key(:type)
+      expect(user[:data][:type]).to be_a(String)
+      expect(user[:data][:type]).to eq("user")
+
+      expect(user[:data]).to have_key(:attributes)
+      expect(user[:data][:attributes]).to have_key(:name)
+      expect(user[:data][:attributes][:name]).to be_a(String)
+
+      expect(user[:data][:attributes]).to have_key(:email)
+      expect(user[:data][:attributes][:email]).to be_a(String)
+
+      expect(user[:data][:attributes]).to have_key(:api_key)
+      expect(user[:data][:attributes][:api_key]).to be_a(String)
+
+      expect(user[:data][:attributes]).to_not have_key(:password)
+      expect(user[:data][:attributes]).to_not have_key(:password_confirmation)
+    end
+  end
+
+  describe "sad paths" do
+    describe "happy path" do
+      it "cannot log in a user with invalid credentials" do
+        user = User.create!(name: "Ringo", email: "starr@ruffruff.com", password: "treats123", password_confirmation: "treats123" )
+
+        user_params = {
+          "email": "starr@ruffruff.com",
+          "password": "treat123"
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post "/api/v1/sessions", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+
+        user = JSON.parse(response.body, symbolize_names: true)
+
+        expect(user).to be_a(Hash)
+        expect(user).to have_key(:error)
+        expect(user[:error]).to be_a(String)
+        expect(user[:error]).to eq("Sorry, your credentials are bad.")
+
+        expect(user).to_not have_key(:data)
+      end
+    end
+  end
+end
