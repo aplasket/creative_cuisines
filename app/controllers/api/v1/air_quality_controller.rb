2621d853dda91f34a57cd8b8f0e1c97cbb2a3e2a
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-07 11:22:15 -0400

    dream coding, fetch capital

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::AirQualityController < ApplicationController
   def index
-    
+    capital = CountryFacade.new(params[:country]).capital
+    # facade = AirQualityFacade.new(capital).air_quality
+    binding.pry
+    # render json: AirQualitySerializer.new(facade)
   end
 end
