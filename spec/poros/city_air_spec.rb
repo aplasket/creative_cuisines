a943334b3ad1e43a3ad44cf721e799e1a6e9bb79
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-07 12:27:58 -0400

    add poro spec

---
+++ spec/poros/city_air_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe CityAir do
+  it "exists and has attributes", :vcr do
+    params = {country: "cuba"}
+    capital = CountryFacade.new(params[:country]).capital
+    object_data = AirQualityFacade.new(capital).air_quality
+
+    expect(object_data).to be_a(CityAir)
+    expect(object_data.aqi).to be_an(Integer)
+    expect(object_data.aqi).to eq(69)
+
+    expect(object_data.co_concentration).to be_a(Float)
+    expect(object_data.co_concentration).to eq(240.33)
+
+    expect(object_data.pm25_concentration).to be_a(Float)
+    expect(object_data.pm25_concentration).to eq(6.57)
+  end
+end
