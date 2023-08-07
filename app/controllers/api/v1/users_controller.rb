8105dcd90b292a6f4a818ce014f3446ce8ec4d90
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-07 17:30:15 -0400

    update errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
     if user.save
       render json: UserSerializer.new(user), status: 201
     else
-      render json: { error: user.errors.full_messages.to_sentence }, status: 400
+      render json: ErrorSerializer.new(user.errors.full_messages.to_sentence, "400").serialized_json, status: 400
     end
   end
 
