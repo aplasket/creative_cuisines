366b9a0627a05cde77edfbe276128ddad4cb6dd2
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-07 13:03:58 -0400

    add sad path to airquality controller for invalid country

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::AirQualityController < ApplicationController
   def index
     capital = CountryFacade.new(params[:country]).capital
-    facade = AirQualityFacade.new(capital).air_quality
-    render json: AirQualitySerializer.new(facade, capital).serialized_json
+    if capital.nil?
+      render json: { error: "invalid country or country does not have a capital" }, status: 400
+    else
+      facade = AirQualityFacade.new(capital).air_quality
+      render json: AirQualitySerializer.new(facade, capital).serialized_json
+    end
   end
 end
