491135201b293ba82e58e018becb175202cc692f
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-07 11:51:59 -0400

    check facade to see objects

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::AirQualityController < ApplicationController
   def index
     capital = CountryFacade.new(params[:country]).capital
-    # facade = AirQualityFacade.new(capital).air_quality
+    facade = AirQualityFacade.new(capital).air_quality
     binding.pry
-    # render json: AirQualitySerializer.new(facade)
+    # render json: AirQualitySerializer.new(facade, capital)
   end
 end
