309dec201ea2e3c75111e421a0aec3a52ecb72c6
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-07 12:14:52 -0400

    add json output for index action

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::AirQualityController < ApplicationController
   def index
     capital = CountryFacade.new(params[:country]).capital
     facade = AirQualityFacade.new(capital).air_quality
-    binding.pry
-    # render json: AirQualitySerializer.new(facade, capital)
+    render json: AirQualitySerializer.new(facade, capital).serialized_json
   end
 end
