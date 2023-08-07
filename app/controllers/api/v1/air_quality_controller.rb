b0d64db6e8a75bdb38649eb5a55687d718056aef
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-07 17:04:51 -0400

    update error messaging

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::AirQualityController < ApplicationController
   def index
     capital = CountryFacade.new(params[:country]).capital
-    if capital.nil?
-      render json: { error: "invalid country or country does not have a capital" }, status: 400
-    else
-      facade = AirQualityFacade.new(capital).air_quality
-      render json: AirQualitySerializer.new(facade, capital).serialized_json
-    end
+    return error unless capital
+
+    facade = AirQualityFacade.new(capital).air_quality
+    render json: AirQualitySerializer.new(facade, capital).serialized_json
+  end
+
+  private
+
+  def error
+    render json: ErrorSerializer.new("Country or Capital Not Found", "404").serialized_json, status: 404
   end
 end
