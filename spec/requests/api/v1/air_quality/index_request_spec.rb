18d404950ee21f6a3ac9a3e80c72afdd2201b8d1
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-07 11:21:09 -0400

    add vcr

--- spec/requests/api/v1/air_quality/index_request_spec.rb
+++ spec/requests/api/v1/air_quality/index_request_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "AirQuality Index Request" do
   describe "happy paths" do
-    it "fetches airquality via country query" do
+    it "fetches airquality via country query", :vcr do
       params = {country: "Nigeria"}
 
       get "/api/v1/air_quality?country=#{params[:country]}"
@@ -12,7 +12,7 @@ RSpec.describe "AirQuality Index Request" do
 
       data = JSON.parse(response.body, symbolize_names: true)
       binding.pry
-      
+
     end
   end
 end
